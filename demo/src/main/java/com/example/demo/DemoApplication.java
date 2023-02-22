09f555f6e0ca2ba835a137d34431216606bd0e79
Er1c0802 <71321119+Er1c0802@users.noreply.github.com>
2023-02-22 16:44:46 +0800

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
 
-        int acca = 23;
+        int accad = 23;
 
         SpringApplication.run(DemoApplication.class, args);
     }
